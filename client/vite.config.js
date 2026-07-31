4718334db4ca7cb25bea2de26d495cf7d9d5fc44
hxrstrike <brightepik@gmail.com>
2026-07-31 06:06:54 +0100

    fix: pin node version, split chunks to eliminate build warnings

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,18 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          charts: ['chart.js', 'react-chartjs-2'],
+          query: ['@tanstack/react-query'],
+        },
+      },
+    },
+  },
   server: {
     port: 3000,
     proxy: {
