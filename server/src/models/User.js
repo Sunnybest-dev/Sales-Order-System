15d32eea5432275814e5b6b93b6cfbe1760b104f
hxrstrike <brightepik@gmail.com>
2026-07-31 02:11:26 +0100

    fix: restore missing name and email fields in User model

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -4,6 +4,8 @@ const sequelize = require('../config/database');
 
 const User = sequelize.define('User', {
   id: { type: DataTypes.CHAR(36), defaultValue: DataTypes.UUIDV4, primaryKey: true },
+  name: { type: DataTypes.STRING(100), allowNull: false },
+  email: { type: DataTypes.STRING(150), allowNull: false, unique: true, validate: { isEmail: true } },
   password: { type: DataTypes.STRING(255), allowNull: false },
   role: {
     type: DataTypes.ENUM('super_admin', 'manager', 'accountant', 'sales_staff'),
